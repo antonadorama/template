68528d4a1b31ab1cc96abb8250091828539d5407
antonadorama <antonadorama@gmail.com>
2017-01-16 12:22:52 -0500

    modified master template

--- store events/content.php
+++ store events/content.php
@@ -3,7 +3,7 @@
    
   <?php //include("store events/test.php"); ?>
     
-  <?php include("store events/banner1.php"); ?>  
+  <?php include("store events/banner4.php"); ?>  
   <?php include("store events/banner2.php"); ?>
   <?php include("store events/banner3.php"); ?>
   
