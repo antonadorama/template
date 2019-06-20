89b68e10b21fd58028a3c00c70ae47809349d7e0
antonadorama <antonadorama@gmail.com>
2019-06-20 16:18:09 -0400

    062019

---
+++ Other/WeekendSummerSale/p_6.php
@@ -0,0 +1,41 @@
+<!-- Two Column Row -->
+
+<tr>
+  <td>
+    <table id="ko_imageBlock_5" style="background-color: #ffffff;" class="vb-outer" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+      <tr>
+        <td class="vb-outer" align="center" valign="top" style="">
+          <!--[if (gte mso 9)|(lte ie 8)]><table align="center" border="0" cellspacing="0" cellpadding="0" width="640"><tr><td align="center" valign="top"><![endif]-->
+          
+          <div class="oldwebkit" style="max-width: 640px;">
+            <table style="border-collapse: separate;padding-left: 0;padding-right: 0;width: 100%;max-width: 640px;background-color: #ffffff;" class="vb-container fullwidth" align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="640">
+              <tbody>
+                <tr>
+                  <td>
+                      <a href="https://www.adorama.com/l/Bags-and-Cases/Camera-Bags-and-Cases?sel=Instant-Rebate_Yes&emailprice=t&utm_source=slgt&refby=slgt&email=##email_address##&utm_medium=email&guid=##GUID##&utm_term=Shop&utm_content=Body&utm_campaign=<?php echo $utm;?>" 
+                         linkname="BagsCases" target="_blank">
+                        <img src="https://www.adorama.com/col/images/email/Optimail/StartOfSummerWeekend/Main/7-Bags-Cases.jpg" 
+                             alt="Bags and Cases" width="100%" height="auto" border="0" style="display:inline;">
+                      </a>
+                  </td>
+                  <td>
+                    <a href="https://www.adorama.com/l/Lens-Filters?sel=Instant-Rebate_Yes&emailprice=t&utm_source=slgt&refby=slgt&email=##email_address##&utm_medium=email&guid=##GUID##&utm_term=Shop&utm_content=Body&utm_campaign=<?php echo $utm;?>" 
+                       linkname="LensFilters" target="_blank">
+                      <img src="https://www.adorama.com/col/images/email/Optimail/StartOfSummerWeekend/Main/8-Lens-Filters.jpg" 
+                           alt="Lens Filters" width="100%" height="auto" border="0" style="display:inline;">
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          
+          <!--[if (gte mso 9)|(lte ie 8)]></td></tr></table><![endif]--></td>
+      </tr>
+    </tbody>
+  </table>
+  </td>
+</tr>  
+
+<!-- End of 2 Column Row -->
